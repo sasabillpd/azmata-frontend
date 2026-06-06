e14ee6e6f92a920a6a3587e5b20d00e623e66f48
Mac <mac@Macs-MacBook-Pro.local>
2026-06-06 12:14:09 +0700

    update API URL

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://azmata-backend-production.up.railway.app/api',
 });
 
 // Auto attach token ke setiap request
